3ee46bb7a81ec328fce34740c6e8242c4bcbef65
sofiacottone <cottonesofia@outlook.it>
2024-08-12 15:37:12 +0200

    feat(typography): add Formula1 font to the project
    - Added F1 font files to the project.
    - Updated stylesheets to include F1 font.
    - Applied F1 font to body tag.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        f1: ['F1', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
