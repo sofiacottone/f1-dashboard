68d6b619b2b8fc46f1fde57695d0982ea350976d
sofiacottone <cottonesofia@outlook.it>
2024-08-12 12:56:53 +0200

    init project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data(){
+    return {}
+  },
+  methods: {
+
+  },
+  mounted(){}
+}
+</script>
+
+<template>
+  <h1 class="text-red-500 text-xl font-bold">Hello World!</h1>
+</template>
---
+++ src/router.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+// import AppHome from './pages/AppHome.vue';
+// import ProjectList from './pages/ProjectList.vue';
+// import ProjectDetails from './pages/ProjectDetails.vue';
+// import NotFound from './pages/NotFound.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        // {
+        //     path: '/',
+        //     name: 'home',
+        //     component: AppHome
+        // },
+        // {
+        //     path: '/projects',
+        //     name: 'projects',
+        //     component: ProjectList
+        // },
+        // {
+        //     path: '/projects/:slug',
+        //     name: 'single-project',
+        //     component: ProjectDetails
+        // },
+        // {
+        //     path: '/:pathMatch(.*)*',
+        //     name: 'not-found',
+        //     component: NotFound
+        // },
+    ]
+});
+export { router };
